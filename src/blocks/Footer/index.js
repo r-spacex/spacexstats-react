73214a555446f0391b427f1567cccda9fc702e86
Albéric Trancart <alberic@trancart.net>
2019-06-16 21:36:56 +0200

    Refactor Footer

--- src/blocks/Footer/index.js
+++ src/blocks/Footer/index.js
@@ -2,82 +2,95 @@ import React from 'react';
 import styled from 'styled-components';
 import { colorUsages } from 'stylesheet';
 
-import { Background } from 'components/ContentBlock';
+import { Background, Wrapper } from 'components/Section';
 import Link from 'components/Link';
 
-const Wrapper = styled(Background)`
+const FooterBackground = styled(Background)`
   display: block;
   height: 100vh;
 `;
 
-const Content = styled.main`
+const WhiteBackground = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  text-align: center;
   height: 100%;
   padding: 1rem;
   background-color: ${colorUsages.footerBackground};
   color: ${colorUsages.footer};
 `;
 
+const FooterWrapper = styled(Wrapper)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`;
+
+const Text = styled.p`
+  margin-bottom: 1rem;
+`;
+
 const Footer = () => (
-  <Wrapper filename="backgrounds/orbcommdark.jpg" tag="footer">
-    <Content id="infos">
-      <p>
-        Photos on this page courtesy SpaceX, &amp; NASA. BFS Hop Test image by{' '}
-        <Link
-          eventLabel="Exit to IanAtkinson_NSF's profile"
-          to="https://www.reddit.com/user/IanAtkinson_NSF"
-          title="IanAtkinson_NSF's Reddit profile"
-        >
-          /u/IanAtkinson_NSF
-        </Link>{' '}
-        on Reddit. All rights maintained by the respective owners.
-        <br />
-        This site is fan-run and not affiliated with SpaceX in any way. For official information and news, please visit{' '}
-        <Link eventLabel="Exit to SpaceX official website" to="http://www.spacex.com" title="Official SpaceX website">
-          www.spacex.com
-        </Link>
-        .
-        <br />
-        Original site concept and design by{' '}
-        <Link
-          eventLabel="Exit to EchoLogic's profile"
-          to="https://www.reddit.com/user/EchoLogic"
-          title="Echologic's Reddit profile"
-        >
-          /u/EchoLogic
-        </Link>
-        , now rehosted by{' '}
-        <Link
-          eventLabel="Exit to Brandtamos' profile"
-          to="https://www.reddit.com/user/brandtamos"
-          title="Brandtamos' Reddit profile"
-        >
-          /u/brandtamos
-        </Link>{' '}
-        and recoded by{' '}
-        <Link
-          eventLabel="Exit to kornelord's profile"
-          to="https://www.reddit.com/user/kornelord"
-          title="kornelord's Reddit profile"
-        >
-          /u/kornelord
-        </Link>{' '}
-        with React and{' '}
-        <Link eventLabel="Exit to r/spacex's API" to="https://github.com/r-spacex/SpaceX-API" title="r/spacex's API">
-          r/spacex’s API
-        </Link>
-        .<br />
-        <br />
-        <Link eventLabel="Exit to Github Repo" to="https://github.com/r-spacex/spacexstats-react" title="Contribute!">
-          GitHub repository
-        </Link>
-      </p>
-    </Content>
-  </Wrapper>
+  <FooterBackground filename="backgrounds/orbcommdark.jpg" tag="footer">
+    <WhiteBackground>
+      <FooterWrapper id="infos">
+        <Text>
+          Photos on this page courtesy SpaceX, NASA and BocaChicaGal. All rights maintained by the respective owners.
+          <br />
+          This site is fan-run and not affiliated with SpaceX in any way. For official information and news, please
+          visit{' '}
+          <Link eventLabel="Exit to SpaceX official website" to="http://www.spacex.com" title="Official SpaceX website">
+            www.spacex.com
+          </Link>
+          .
+        </Text>
+        <Text>
+          Original site concept and design by /u/EchoLogic, now recoded by /u/kornelord with{' '}
+          <Link eventLabel="Exit to GatsbyJS website" to="https://www.gatsbyjs.org/" title="GatsbyJS">
+            GatsbyJS
+          </Link>{' '}
+          and{' '}
+          <Link eventLabel="Exit to r/spacex's API" to="https://github.com/r-spacex/SpaceX-API" title="r/spacex's API">
+            r/spacex’s API
+          </Link>
+          .<br />
+          Contact for feedback:{' '}
+          <Link
+            eventLabel="Exit to kornelord's profile"
+            to="https://www.reddit.com/user/kornelord"
+            title="kornelord's Reddit profile"
+          >
+            /u/kornelord on Reddit
+          </Link>
+          ,{' '}
+          <Link
+            eventLabel="Exit to Albéric Trancart's profile"
+            to="https://twitter.com/alberictrancart"
+            title="kornelord's Twitter profile"
+          >
+            Albéric Trancart on Twitter
+          </Link>
+          .
+        </Text>
+        <Text>
+          Domain name rehosted by{' '}
+          <Link
+            eventLabel="Exit to Brandtamos' profile"
+            to="https://www.reddit.com/user/brandtamos"
+            title="Brandtamos' Reddit profile"
+          >
+            /u/brandtamos
+          </Link>
+          .
+          <br />
+          <Link eventLabel="Exit to Github Repo" to="https://github.com/r-spacex/spacexstats-react" title="Contribute!">
+            GitHub repository
+          </Link>
+        </Text>
+      </FooterWrapper>
+    </WhiteBackground>
+  </FooterBackground>
 );
 
 export default Footer;
