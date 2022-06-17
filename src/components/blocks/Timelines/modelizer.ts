f433630432f98c971eda320ed56fe793ab4c74e8
MySurmise <marlbos@outlook.de>
2022-06-17 15:51:34 +0200

    added new Shotwell bet

--- src/components/blocks/Timelines/modelizer.ts
+++ src/components/blocks/Timelines/modelizer.ts
@@ -4,10 +4,12 @@ export interface ModelizedSectionData {
   elonMuskBet: Date;
   gwynneShotwellBet: Date;
   foundingDate: Date;
+  gwynneShotwellMarsBet: Date;
 }
 
 export const modelizer = (_: SpaceXData): ModelizedSectionData => ({
   elonMuskBet: new Date('2026-01-01'),
   gwynneShotwellBet: new Date('2029-01-01'),
   foundingDate: new Date('2002-03-14'),
+  gwynneShotwellMarsBet: new Date('2032-05-24'),
 });
